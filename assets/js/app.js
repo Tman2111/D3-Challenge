b70a0dca691b244c78b62f76fcc4196d8535ce0b
Tman <76560394+Tman2111@users.noreply.github.com>
2021-08-09 19:23:48 -0400

    Parse and examine data

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,41 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+// SVG wrapper
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+  // append SVG group
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  (async function(){
+    var censusData = await d3.csv("assets/data/data.csv").catch(err => console.log(err))
+
+    // Parse data
+    censusData.forEach(function(data) {
+      data.id = +data.id;
+      data.poverty = +data.poverty;
+      data.povertyMoe = +data.povertyMoe;
+      data.age = +data.age;
+      data.ageMoe = +data.ageMoe;
+      data.income = +data.income;
+      data.incomeMoe = +data.incomeMoe;
+      data.healthcare = +data.healthcare;
+      data.healthcareLow = +data.healthcareLow;
+      data.healthcareHigh = +data.healthcareHigh;
+      data.obesity = +data.obesity;
+      data.obesityLow = +data.obesityLow;
+      data.obesityHigh = +data.obesityHigh;
+      data.smokes = +data.smokes;
+      data.smokesLow = +data.smokesLow;
+      data.smokesHigh = +data.smokesHigh;
+    });
+
+    console.log(censusData);
+})() 
+
